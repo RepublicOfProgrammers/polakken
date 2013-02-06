2ffcbb559f983f18042f436cec3a3f0185485da1
Sondre Jensen <situz.sj@gmail.com>
2013-02-07 00:29:02 +0100

    First!!11!11

--- Polakken/Program.cs
+++ Polakken/Program.cs
@@ -17,6 +17,7 @@ namespace Polakken
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
