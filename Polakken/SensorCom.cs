e10555b38144fe952fe12656d2fc3deaa5ba92c4
Sondre Jensen <situz.sj@gmail.com>
2013-03-06 11:39:58 +0100

    Lagt inn nidaq c# wrapperen (niHSDIO.cs) og implementert denne i SensorCom.cs

--- Polakken/SensorCom.cs
+++ Polakken/SensorCom.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using InstrumentDriverInterop.Ivi;
+
 
 namespace Polakken
 {
     class SensorCom
-    {
+    {        
+		niHSDIO my_niHSDIO; //niHSDIO object
+        string Sensor_name = "0xxxxx"; //navn til sensoren
+        string Sensor_channel = "x"; //kanal sensor benytter
+
+        private void initReader() 
+        {
+            //variable declaration of local
+            uint data;
+
+            my_niHSDIO = InstrumentDriverInterop.Ivi.niHSDIO.InitAcquisitionSession(Sensor_name, true, true, "");
+            //initializing niHSDIO object
+            my_niHSDIO.AssignStaticChannels(Sensor_channel);
+            //configure acquisition with channels listed on form
+            my_niHSDIO.ReadStaticU32(out data);	//Leser temp, putter temp i "data" variabel
+            my_niHSDIO.Dispose(); //dispose of handle
+        }
     }
 }
+
