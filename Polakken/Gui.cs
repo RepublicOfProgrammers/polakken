aa2bf299b4c31eec8474cd5de58fa5b4ac0a5cee
Alexander <alexandergjerseth@gmail.com>
2013-03-06 12:43:34 +0100

    hei

--- Polakken/Gui.cs
+++ Polakken/Gui.cs
@@ -22,64 +22,59 @@ namespace Polakken
 
             // Graf:
 
-            crtOversikt.ChartAreas.Add("tempOversikt");
-            crtOversikt.ChartAreas["tempOversikt"].AxisX.Minimum = 0;
-            crtOversikt.ChartAreas["tempOversikt"].AxisX.Maximum = 20;
-            crtOversikt.ChartAreas["tempOversikt"].AxisX.Interval = 1;
-            crtOversikt.ChartAreas["tempOversikt"].AxisY.Minimum = -30;
-            crtOversikt.ChartAreas["tempOversikt"].AxisY.Interval = 10;
-            crtOversikt.ChartAreas["tempOversikt"].BackColor = Color.Black;
-            crtOversikt.ChartAreas["tempOversikt"].AxisX.MajorGrid.LineColor = Color.DarkGray;
-            crtOversikt.ChartAreas["tempOversikt"].AxisY.MajorGrid.LineColor = Color.DarkGray;
-            crtOversikt.ChartAreas["tempOversikt"].AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
-            crtOversikt.ChartAreas["tempOversikt"].AxisX.LabelStyle.ForeColor = Color.Yellow;
-            crtOversikt.ChartAreas["tempOversikt"].AxisX.LineColor = Color.DarkGray;
-            crtOversikt.ChartAreas["tempOversikt"].AxisY.LineColor = Color.DarkGray;
-            crtOversikt.ChartAreas["tempOversikt"].AxisY.LabelStyle.ForeColor = Color.Yellow;
+            crtView.ChartAreas.Add("tempOversikt");
+            crtView.ChartAreas["tempOversikt"].AxisX.Minimum = 0;
+            crtView.ChartAreas["tempOversikt"].AxisX.Maximum = 20;
+            crtView.ChartAreas["tempOversikt"].AxisX.Interval = 1;
+            crtView.ChartAreas["tempOversikt"].AxisY.Minimum = -30;
+            crtView.ChartAreas["tempOversikt"].AxisY.Interval = 10;
+            crtView.ChartAreas["tempOversikt"].BackColor = Color.Transparent;
+            crtView.ChartAreas["tempOversikt"].AxisX.MajorGrid.LineColor = Color.DarkGray;
+            crtView.ChartAreas["tempOversikt"].AxisY.MajorGrid.LineColor = Color.DarkGray;
+            crtView.ChartAreas["tempOversikt"].AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
+            crtView.ChartAreas["tempOversikt"].AxisX.LabelStyle.ForeColor = Color.GreenYellow;
+            crtView.ChartAreas["tempOversikt"].AxisX.LineColor = Color.DarkGray;
+            crtView.ChartAreas["tempOversikt"].AxisY.LineColor = Color.DarkGray;
+            crtView.ChartAreas["tempOversikt"].AxisY.LabelStyle.ForeColor = Color.GreenYellow;
             
             
             
-            crtOversikt.Series.Add("Pakistan");
-            crtOversikt.Series.Add("India");
+            crtView.Series.Add("Pakistan");
+            crtView.Series.Add("India");
 
-            crtOversikt.Series["Pakistan"].Color = Color.Green;
-            crtOversikt.Series["India"].Color = Color.Red;
+            crtView.Series["Pakistan"].Color = Color.LawnGreen;
+            crtView.Series["India"].Color = Color.Red;
 
-            crtOversikt.Series["Pakistan"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            crtOversikt.Series["India"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            crtOversikt.Series["Pakistan"].BorderWidth = 2;
-            crtOversikt.Series["India"].BorderWidth = 2;
+            crtView.Series["Pakistan"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            crtView.Series["India"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            crtView.Series["Pakistan"].BorderWidth = 2;
+            crtView.Series["India"].BorderWidth = 2;
             
                 
 
-            crtOversikt.Series["Pakistan"].Points.AddXY(0, 0);
-            crtOversikt.Series["India"].Points.AddXY(0, 0);
+            crtView.Series["Pakistan"].Points.AddXY(0, 0);
+            crtView.Series["India"].Points.AddXY(0, 0);
 
-            crtOversikt.Series["Pakistan"].Points.AddXY(1, 4);
-            crtOversikt.Series["India"].Points.AddXY(1, 9);
+            crtView.Series["Pakistan"].Points.AddXY(1, 4);
+            crtView.Series["India"].Points.AddXY(1, 9);
 
-            crtOversikt.Series["Pakistan"].Points.AddXY(12, -10);
-            crtOversikt.Series["India"].Points.AddXY(12, 20);
+            crtView.Series["Pakistan"].Points.AddXY(12, -10);
+            crtView.Series["India"].Points.AddXY(12, 20);
 
-            crtOversikt.Series["Pakistan"].Points.AddXY(13, 5);
-            crtOversikt.Series["India"].Points.AddXY(13, 15);
+            crtView.Series["Pakistan"].Points.AddXY(13, 5);
+            crtView.Series["India"].Points.AddXY(13, 15);
 
-            crtOversikt.Series["Pakistan"].Points.AddXY(15, -100);
-            crtOversikt.Series["India"].Points.AddXY(15, -34);
+            crtView.Series["Pakistan"].Points.AddXY(15, -100);
+            crtView.Series["India"].Points.AddXY(15, -34);
 
-            crtOversikt.Series["Pakistan"].Points.AddXY(16, -50);
-            crtOversikt.Series["India"].Points.AddXY(16, -10);
+            crtView.Series["Pakistan"].Points.AddXY(16, -50);
+            crtView.Series["India"].Points.AddXY(16, -10);
 
-           
 
+            txtCurrent.AppendText("40" + ");
          
 
 
-            //Tekstboksen:
-
-            txtSiste.BackColor = Color.Black;
-            txtSiste.ForeColor = Color.Green;
-            txtSiste.AppendText("50");
             
 
             
@@ -94,5 +89,8 @@ namespace Polakken
         {
 
         }
+
+ 
+     
     }
 }
