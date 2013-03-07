eda806073337a9d0175817e312500cbb0e42af76
Vaglefant <stian_glittum5@msn.com>
2013-03-07 13:23:25 +0100

    E-mail

---
+++ Polakken/E-mail handler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+
+namespace Polakken
+{
+    class E_mail_handler
+    {
+
+    }
+}
