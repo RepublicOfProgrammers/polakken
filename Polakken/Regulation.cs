6caf7b8a0b22bdf15d0665bb3bf83b11f8c45301
Morten Hafredal <mhafredal@hotmail.com>
2013-03-13 09:59:36 +0100

    c

--- Polakken/Regulation.cs
+++ Polakken/Regulation.cs
@@ -7,53 +7,60 @@ namespace Polakken
 {
     class Regulation
     {
-        ////variabler
-        //private Boolean status;
-        //private double reading;
-        //private double prevReading;
-        //private double tolerance;
-        //public double setpoint { get; set; }
-        //private double difference;
-        ////temp variabler
-        //private double mesInterval;
-        //private double redingFraMes;
-        
-        ////ny måling
-        //public void newread(double newread)
-        //{
-        //    prevReading = reading;
-        //    reading = newread;
-        //}
+        //variabler
+        private Boolean status;
+        private double reading;
+        private double prevReading;
+        public double tolerance { get; set; }
+        public double setpoint { get; set; }
+        private double difference;
+        private double mesInterval;
 
-        ////regulering
-        //public Boolean reg()
-        //{
-        //    //temperatur under min toleranse, skrur på ovnen. 
-        //    if ((reading + tolerance) < setpoint) 
-        //    {
-        //        status = true;
-        //    }
-        //    //temperatur over max toleranse, skrur av ovnen. 
-        //    else if ((reading - tolerance) > setpoint) 
-        //    {
-        //        status = false;
-        //    }
-        //    //sørger for at ovnen startes opp før temperaturen blir under toleransen. 
-        //    else if ((prevReading - reading) < (setpoint - prevReading))
-        //    {
-        //        difference = (prevReading - reading) / (setpoint - prevReading);
-        //        System.Threading.Thread.Sleep(Convert.ToInt32(difference * mesInterval)); //må omgjøres slik at intervallet blir i millisekunder. 
-        //        status = true;
-        //    }
-        //    //sørger for at ovnen stoppes før temperaturen blir over toleransen. 
-        //    else if ((reading - prevReading) < (prevReading - setpoint))
-        //    {
-        //        difference = (reading - prevReading) / (prevReading - setpoint);
-        //        System.Threading.Thread.Sleep(Convert.ToInt32(difference * mesInterval)); //må omgjøre slit at intervallet blir i millisekunder. 
-        //    }
+        //constructor
+        public Regulation(double setpoint, double tolerance)
+        {
+            this.setpoint = setpoint;
+            this.tolerance = tolerance;
+            //inn måleverdi fra sensorcom
+            //prevReading = reading;
+        }
 
-        //    return status;
-        //}
+        //ny måling
+        public void newread(double newread)
+        {
+            prevReading = reading;
+            reading = newread;
+        }
+
+        //regulering
+        public Boolean regulator()
+        {
+            //temperatur under min toleranse, skrur på ovnen. 
+            if ((reading + tolerance) < setpoint)
+            {
+                status = true;
+            }
+            //temperatur over max toleranse, skrur av ovnen. 
+            else if ((reading - tolerance) > setpoint)
+            {
+                status = false;
+            }
+            //sørger for at ovnen startes opp før temperaturen blir under toleransen. 
+            else if ((prevReading - reading) < (setpoint - prevReading))
+            {
+                difference = (prevReading - reading) / (setpoint - prevReading);
+                System.Threading.Thread.Sleep(Convert.ToInt32(difference * mesInterval)); //må omgjøres slik at intervallet blir i millisekunder. 
+                status = true;
+            }
+            //sørger for at ovnen stoppes før temperaturen blir over toleransen. 
+            else if ((reading - prevReading) < (prevReading - setpoint))
+            {
+                difference = (reading - prevReading) / (prevReading - setpoint);
+                System.Threading.Thread.Sleep(Convert.ToInt32(difference * mesInterval)); //må omgjøre slik at intervallet blir i millisekunder. 
+            }
+
+            return status;
+        }
 
     }
 }
