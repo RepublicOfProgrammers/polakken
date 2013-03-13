52cc9b0d50e60b6a1dfb1fe6c928259e04e371ae
Vaglefant <stian_glittum5@msn.com>
2013-03-13 19:14:04 +0100

    gsdfg

--- Polakken/Gui.cs
+++ Polakken/Gui.cs
@@ -176,12 +176,22 @@ namespace Polakken
         private void send_Click(object sender, EventArgs e)
         {
             E_mail_handler email = new E_mail_handler();
-
-            email.client.Send("republicofprogrammers@gmail.com", txtEmail1.Text, "Hei", "Hei");
-            email.client.Send("republicofprogrammers@gmail.com", txtEmail2.Text, "Hei", "Hei");
-            email.client.Send("republicofprogrammers@gmail.com", txtEmail3.Text, "Hei", "Hei");
-            email.client.Send("republicofprogrammers@gmail.com", txtEmail4.Text, "Hei", "Hei");
-
+            if (txtEmail1.Text != "")
+            {
+                email.client.Send("republicofprogrammers@gmail.com", txtEmail1.Text, "Hei", "Hei");
+            }
+            if (txtEmail2.Text != "")
+            {
+                email.client.Send("republicofprogrammers@gmail.com", txtEmail2.Text, "Hei", "Hei");
+            }
+            if (txtEmail3.Text != "")
+            {
+                email.client.Send("republicofprogrammers@gmail.com", txtEmail3.Text, "Hei", "Hei");
+            }
+            if (txtEmail4.Text != "")
+            {
+                email.client.Send("republicofprogrammers@gmail.com", txtEmail4.Text, "Hei", "Hei");
+            }
             MessageBox.Show("Mailen har blitt sendt");
         }
 
