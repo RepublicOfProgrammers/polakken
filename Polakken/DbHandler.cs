629f1b1b5ec18d544e68febec1abe34ab56dea11
Fredrik <facelift77@hotmail.com>
2013-04-03 12:29:04 +0200

    delReadings fix

--- Polakken/DbHandler.cs
+++ Polakken/DbHandler.cs
@@ -119,7 +119,7 @@ namespace Polakken
         
         public int DelReadings(DateTime fraDato, DateTime tilDato) //Sletter oppføringer i temp-databasen fra-til dato
         {
-            string sql = string.Format("delete from {0} where {1} < {2} and {1} > {3}", TB_READINGS, TB_READINGS_DATE, tilDato, fraDato);
+            string sql = string.Format("delete from {0} where {1} < '{2}' and {1} > '{3}'", TB_READINGS, TB_READINGS_DATE, tilDato, fraDato);
             return executeSql_NonQuery(sql);
         }
 
