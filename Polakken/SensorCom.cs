d1b45854fa34c8945b25b9043b15c78903c34a50
Morten Hafredal <mhafredal@hotmail.com>
2013-04-03 12:36:46 +0200

    Småfiksing i Regulation og SensorCom

--- Polakken/SensorCom.cs
+++ Polakken/SensorCom.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-//using InstrumentDriverInterop.Ivi;
 using NationalInstruments;
 using NationalInstruments.DAQmx;
 
@@ -11,34 +10,33 @@ namespace Polakken
 {
     class SensorCom
     {
-        //To do list:
-        //Built-in commando for å hente ut CJC-value til termoelementet
-        //Feilsøking for å oppdage hvis det skulle bli feil ved måleren (brutt signal)
         public double mesInterval {get; set;}
-        public double cjc { get; set; }
-
-        public SensorCom(double mesInterval, double cjcValue)
-        {
-            this.mesInterval = mesInterval;
-            cjc = cjcValue;
-        }
 
         public SensorCom(double mesInterval)
         {
             this.mesInterval = mesInterval;
-            cjc = 30;
         }
 
         public double temp()
-        {            
-            Task temperatureTask = new Task();
-            AIChannel myAIChannel;
-            myAIChannel = temperatureTask.AIChannels.CreateThermocoupleChannel("Dev1/ai0", "Temperature", 0, 100,
-                AIThermocoupleType.J, AITemperatureUnits.DegreesC, cjc);
+        {
+            try
+            {
+                //Debug.WriteLine("<<TempDebug>>");
+                Task temperatureTask = new Task();
+                AIChannel myAIChannel;
+                myAIChannel = temperatureTask.AIChannels.CreateThermocoupleChannel("Dev1/ai0", "Temperature", 0, 100,
+                    AIThermocoupleType.J, AITemperatureUnits.DegreesC);
 
-            AnalogSingleChannelReader reader = new AnalogSingleChannelReader(temperatureTask.Stream);
-            double analogData = reader.ReadSingleSample();
-            return analogData;
+                AnalogSingleChannelReader reader = new AnalogSingleChannelReader(temperatureTask.Stream);
+                double analogData = reader.ReadSingleSample();
+                //Debug.WriteLine(analogData);
+                return analogData;
+            }
+            catch (Exception e)
+            {
+                return 999;
+                //999 vil være en "feilkode", exceptionen bør også logges. 
+            }
         }
     }
 }
