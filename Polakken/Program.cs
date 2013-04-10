57e6771de9fd7d027323bea724f1f2a9e2c55c5b
Sondre <situz.sj@gmail.com>
2013-04-10 14:19:24 +0200

    Polakken har nå fått en tråd som kjører målinger ved intervall fra instillinger. (i teorien)

--- Polakken/Program.cs
+++ Polakken/Program.cs
@@ -2,31 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Threading;
+using Polakken.Properties;
 
 namespace Polakken
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        private static DbHandler mDbHandler;
+
         [STAThread]
         static void Main()
         {
             new Logger(); // kaller konstruktøren til logger classen kun for å opprette ny logg tekstfil. 
+            mDbHandler = new DbHandler(); // Fungerer som en sjekk på at databasen fungerer. brukes også i tråden for tempmåling tMålTemp_method()
 
-            //TODO: hente inn config settpunkt på måleintervall for å sende inn til SensorCom
-            //TODO: kalle SensorCom med måleintervall
-            //TODO: Kalle dbhandler.Oppstartsjekk() (denne må også lages, skal sjekke om alt er i orden med harddisk)
+            SensorCom.mesInterval = Settings.Default.mesInterval; // henter inn config settpunkt på måleintervall og sender til SensorCom
+            SensorCom.alarmLimit = Settings.Default.alarmLimit; // Henter inn config settpunk på alarmgrense og sender til SensorCom
+
+            Thread tMålTemp = new Thread(new ThreadStart(tMålTemp_method));
+            tMålTemp.Start(); // Starter måleprosessen. Main() venter ikke på denne tråden før den går videre.
+
             //TODO: Sjekke sist innlogging via databasen. Rapportere til bruker at konfigurasjoner er i orden, polakken starter arbeid.
-
-
-            //Eventuelt, sletting av eldgammel data som ikke trengs og som tar opp plass/resusser.
-
+            //Eventuelt TODO: sletting av eldgammel data som ikke trengs og som tar opp plass/resusser.
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GUI());
+        }
+
+        /// <summary>
+        /// Denne metoden for tråden tMålTemp vil skrive en måling til databasen med datotid, temp og status på varmeovner. Deretter vil tråden sove frem til måleintervallet(mesIntervall) er utløpt
+        /// </summary>
+        private static void tMålTemp_method()
+        {
+            if ((int)SensorCom.temp() == 999)
+            {
+                Logger.Warning("Får ikke kontakt med måleenhet (se foregående error fra SensorCom), skriver ikke til database, Polakken blunder en times tid.", "Polakken");
+                //Sende email?
+                Thread.Sleep(3600000);
+            }
+            else
+            {
+                mDbHandler.SetReading(DateTime.Now, (int)SensorCom.temp(), GUI.test);
+                Logger.Info("Utført måling, og skrevet til database.", "Polakken");
+                Thread.Sleep(SensorCom.mesInterval);
+            }
         }
     }
 }
