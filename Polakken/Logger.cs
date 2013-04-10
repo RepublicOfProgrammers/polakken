74284f01132375c44732914ec8b5f4c8c4f42ca8
Sondre <situz.sj@gmail.com>
2013-04-10 12:14:29 +0200

    Kommentering på logger klassen. Startet arbeid i program klassen(polakken)

--- Polakken/Logger.cs
+++ Polakken/Logger.cs
@@ -6,19 +6,25 @@ namespace Polakken
 {
     public class Logger
     {
+        //Mappen hvor alle log filene skal ligge
         private string dirLogs = "Logs";
+
+        //Konstruktør som kun brukes i oppstart av programmet for å opprette en ny logg fil med dagens dato
         public Logger() 
         {
             if (!Directory.Exists(dirLogs)) 
             {
+                //Dersom mappen ikke eksisterer så opprettes den
                 Directory.CreateDirectory(dirLogs);
             }
+            // Oppretter filen med unikt navn
             string filename = string.Format("{0}/Programstart {1}.log", dirLogs, DateTime.Now.ToString("dd-MM-yyy HH.mm.ss"));
+            //Legger filen til i Listeners-listen til programkjøringen. Denne lytter etter trace-events, som behandles i WriteEntry()
             Trace.Listeners.Add(new TextWriterTraceListener(filename, "PolakkenLytter"));
             Trace.WriteLine("Tidspunkt \tType \tKlasse \t\tMelding");
             Trace.WriteLine("-------------------------------------------------------------");
         }
-
+        //Følgende metoder brukes i de forskjellige trace event'ene vi har definert.
         public static void Error(string message, string module)
         {
             WriteEntry(message, "FEIL", module);
@@ -41,12 +47,14 @@ namespace Polakken
 
         private static void WriteEntry(string message, string type, string module)
         {
+            //Skriver til trace, som deretter blir skrev til tekstfilen via listeneren. 
             Trace.WriteLine(
                     string.Format("{0} \t{1} \t{2}: \t{3}",
                                   DateTime.Now.ToString("HH:mm:ss"),
                                   type,
                                   module,
                                   message));
+            //Flusher til slutt trace, så ikke ting blir ført opp dobbelt.
             Trace.Flush();
         }
     }
