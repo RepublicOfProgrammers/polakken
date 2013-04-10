c2edacca179d0cb7bb04afb90f7f29098168b897
Fredrik <facelift77@hotmail.com>
2013-04-10 15:09:35 +0200

    messagebox for feilmeldinger

--- Polakken/Program.cs
+++ Polakken/Program.cs
@@ -17,11 +17,14 @@ namespace Polakken
             new Logger(); // kaller konstruktøren til logger classen kun for å opprette ny logg tekstfil. 
             mDbHandler = new DbHandler(); // Fungerer som en sjekk på at databasen fungerer. brukes også i tråden for tempmåling tMålTemp_method()
 
-            SensorCom.mesInterval = Settings.Default.mesInterval; // henter inn config settpunkt på måleintervall og sender til SensorCom
-            SensorCom.alarmLimit = Settings.Default.alarmLimit; // Henter inn config settpunk på alarmgrense og sender til SensorCom
+            //SensorCom.mesInterval = Settings.Default.mesInterval; // henter inn config settpunkt på måleintervall og sender til SensorCom
+            //SensorCom.alarmLimit = Settings.Default.alarmLimit; // Henter inn config settpunk på alarmgrense og sender til SensorCom
 
             Thread tMålTemp = new Thread(new ThreadStart(tMålTemp_method));
             tMålTemp.Start(); // Starter måleprosessen. Main() venter ikke på denne tråden før den går videre.
+            
+            //Thread polakkenLytter = new Thread(new ThreadStart(polakkenLytter_Method));
+            //polakkenLytter.Start();
 
             //TODO: Sjekke sist innlogging via databasen. Rapportere til bruker at konfigurasjoner er i orden, polakken starter arbeid.
             //Eventuelt TODO: sletting av eldgammel data som ikke trengs og som tar opp plass/resusser.
@@ -36,18 +39,31 @@ namespace Polakken
         /// </summary>
         private static void tMålTemp_method()
         {
-            if ((int)SensorCom.temp() == 999)
+            while (true)
             {
-                Logger.Warning("Får ikke kontakt med måleenhet (se foregående error fra SensorCom), skriver ikke til database, Polakken blunder en times tid.", "Polakken");
-                //Sende email?
-                Thread.Sleep(3600000);
-            }
-            else
-            {
-                mDbHandler.SetReading(DateTime.Now, (int)SensorCom.temp(), GUI.test);
-                Logger.Info("Utført måling, og skrevet til database.", "Polakken");
-                Thread.Sleep(SensorCom.mesInterval);
+                if ((int)SensorCom.temp() == 999)
+                {
+                    Logger.Warning("Får ikke kontakt med måleenhet (se foregående error fra SensorCom), skriver ikke til database, Polakken blunder en times tid.", "Polakken");
+                    //Sende email?
+                    Thread.Sleep(3600000);
+                }
+                else
+                {
+                    mDbHandler.SetReading(DateTime.Now, (int)SensorCom.temp(), GUI.test);
+                    Logger.Info("Utført måling, og skrevet til database.", "Polakken");
+                    Thread.Sleep(SensorCom.mesInterval);
+                }
             }
         }
+
+        //private static void polakkenLytter_Method()
+        //{
+        //    while (true)
+        //    {
+        //        string message = "noe piss";
+        //        MessageBox.Show(message);
+        //        Thread.Sleep(1500);
+        //    }   
+        //}
     }
 }
