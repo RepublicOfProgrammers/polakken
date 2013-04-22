398df2fcfee89a2498c413f2604114abc0f96118
Vaglefant <stian_glittum5@msn.com>
2013-04-22 16:46:47 +0200

    Endra email handler til sendMail

--- Polakken/sendMail.cs
+++ Polakken/sendMail.cs
@@ -1,45 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Net;
 using System.Net.Mail;
-using System.Data.SqlServerCe;
-using System.Diagnostics;
 using System.Data;
 
 namespace Polakken
 {
     static class E_mail_handler
     {
-        private static string host = "smtp.gmail.com";
-        private static int port = 587;
-        private static string username = "republicofprogrammers@gmail.com";
+        private static string email = "republicofprogrammers@gmail.com";
         private static string password = "polakken";
         private static string module = "E-mail handler";
 
-
-        public static  SmtpClient client = new SmtpClient(host, port) //Lager en ny SmtpClient med host-navn og port
+        public static  SmtpClient client = new SmtpClient("smtp.gmail.com", 587) //Lager en ny SmtpClient med host-navn og port
          {
-             Credentials = new NetworkCredential(username, password), //Login-informasjon for emailen vi sender fra
+             Credentials = new NetworkCredential(email, password), //Login-informasjon for emailen vi sender fra
              EnableSsl = true //Legger til sikkerhetslaget Ssl
          };
-        
 
         //Metode som sender mail til alle som er oppført i databasen
         public static void sendToAll(string subject, string body)
         {
             try
             {
-
-
                 string mailTil;
 
-                //Løkke som går gjennom databasen for å hente ut alle e-mails
                 foreach (DataRow dtRow in GUI.dtEmails.Rows)
                 {
                     mailTil = dtRow["Adresser"].ToString();
-                    client.Send(username, mailTil, subject, body);
+                    client.Send(email, mailTil, subject, body);
                     Logger.Info("Sendt email til alle mottakere." ,module);
                 }
             }
@@ -55,7 +43,7 @@ namespace Polakken
         {
             try
             {
-                client.Send(username, mailAdress, subject, body);
+                client.Send(email, mailAdress, subject, body);
                 Logger.Info("Sendt email til " + mailAdress + ".", module);
             }
             catch (Exception ex)
