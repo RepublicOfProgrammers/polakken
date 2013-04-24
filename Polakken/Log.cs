9c2fb55f67ce14d701395517cf63fd447cf3981a
Sondre <situz.sj@gmail.com>
2013-04-24 11:47:23 +0200

    Siste warning vekk fra log

--- Polakken/Log.cs
+++ Polakken/Log.cs
@@ -45,30 +45,39 @@ namespace Polakken
 
         private void tmrUpdateText_Tick(object sender, EventArgs e)
         {
-            var fs = new FileStream(Logger.currentLog, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            using (var sr = new StreamReader(fs))
+            tmrUpdateText.Start();
+            FileStream fs = null;
+            try
             {
-
-                txtRead.Text = sr.ReadToEnd();
-                txtRead.SelectionStart = txtRead.Text.Length;
-                txtRead.ScrollToCaret();
-                
-              
+                fs = new FileStream(Logger.currentLog, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    fs = null;
+                    txtRead.Text = sr.ReadToEnd();
+                    txtRead.SelectionStart = txtRead.Text.Length;
+                    txtRead.ScrollToCaret();
+                }
+            }
+            catch (Exception) { }
+            finally
+            {
+                if (fs != null)
+                    fs.Dispose();
             }
         }
       
         private void Log_Load(object sender, EventArgs e)
         {
             tmrUpdateText.Start();
-            var fs = (FileStream)null;
+            FileStream fs = null;
             try
             {
                 fs = new FileStream(Logger.currentLog, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-                using (var sr = new StreamReader(fs))
+                using (StreamReader sr = new StreamReader(fs))
                 {
+                    fs = null;
                     txtRead.Text = sr.ReadToEnd();
                 }
-                fs = null;
             }
             catch (Exception) { }
             finally
